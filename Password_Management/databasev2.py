0900d3e65e0100df732c07eeae2cb23c3ed57832
Lalit Jagotra <lalit.jagotra@gmail.com>
2025-03-08 23:09:36 +0530

    Update databasev2.py
    Improvements in secure password storage

--- Password_Management/databasev2.py
+++ Password_Management/databasev2.py
@@ -99,34 +99,16 @@ class database:
         self.cursor = self.db.execute(query)
         return self.cursor.fetchone()[0]
 
-''' This is the main program to test this interface performing the basic crud operation'''
+''' This is the main program to test this interface performing the basic crud operation----'''
 def main():
-##    db = database(filename='C:/Python_Files/Password_Management/login',table='accounts')
-##    db.connect_database()
-##    query = 'drop table if exists test'
-##    db.create_table(dict(ROW_ID= "int", username= "char", password= "char",applicationname="char", applicationurl="char"))
-##    db.insert(dict(ROW_ID = 101, username = 'user1', password = 'password1',applicationname = 'GMAIL', applicationurl = 'https://accounts.google.com/'))
-##    db.insert(dict(ROW_ID = 102, username = 'user2', password = 'password2',applicationname = 'GMAIL', applicationurl = 'https://accounts.google.com/'))
-##    db.delete(104)
-##    print(db.retrieve_rows())                
-##    #db.update(102, {username : 'user3', password : 'password3',role : 'non-admin', membertype : 'permanent'})
-##    print("table after update")
-##    print(db.retrieve_rows())
-##    print("this the one{}".format(db.retrieve_row(101)))
-##    count = db.countrecs()
-##    print (count)
     dbdir=os.getcwd()
     db1 = database(filename= dbdir + '\\authenticate',table='users')
     db1.connect_database()
     query = 'drop table if exists users'
     db1.sql_noparam(query)
-    db1.create_table(dict(ROW_ID= "int", username= "char", password= "char", masterenc="char"))
-##    dt=timedelta(milliseconds=1)
-##    print(dt.total_seconds())
-    db1.insert(dict(ROW_ID = 101, username = 'user1.login@domain.com', password = 'Secure_Password_1234', masterenc=None))
-    db1.insert(dict(ROW_ID = 102, username = 'user2@domain.com', password = 'password2', masterenc=None))
-    print(db1.retrieve_rows())
+    db1.create_table(dict(ROW_ID= "int", username= "char", password= "varchar", masterenc="char"))
     usertabledb=str()
+    print("---------------------All Databases are re-initialized------------------------------")
     for item in db1.retrieve_rows():
             usertable=(item['username']).split('@')
             for item1 in usertable:
@@ -137,13 +119,9 @@ def main():
                     usertabledb+=item1
             db2 = database(filename=dbdir+ '\\login',table=usertabledb)
             db2.connect_database()
-            #query = 'drop table if exists ?'
-            #db2.sql_do(query,(item['username'].strip("@")))
+            query = 'drop table if exists ' + usertabledb
+            db1.sql_noparam(query)
             db2.create_table(dict(ROW_ID= "int", username= "char", password= "char",applicationname="char", applicationurl="char"))
-            #db2.insert(dict(ROW_ID = 101, username = item['username'], password = 'password1',applicationname = 'GMAIL', applicationurl = 'https://accounts.google.com/'))
-            #db2.insert(dict(ROW_ID = 102, username = 'user2@domain.com', password = 'password2',applicationname = 'GMAIL', applicationurl = 'https://accounts.google.com/'))
-            #print(db2.retrieve_rows())
-            print("---------------------------------------------------")
             usertabledb=str()
-    #print(db2.retrieve_row(101))
+
 if __name__ == '__main__':main()
